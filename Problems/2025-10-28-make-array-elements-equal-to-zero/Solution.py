a7142d8356ad140f90f818cee425169a4f9a0587
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2025-11-05 11:51:28 +0530

    Day 1: solved 3354 - Make Array Elements Equal to Zero

---
+++ Problems/2025-10-28-make-array-elements-equal-to-zero/Solution.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        def simulate(start_pos, direction):
+            arr = nums[:]
+            n = len(arr)
+            curr = start_pos
+            dir = direction
+            
+            while 0 <= curr < n:
+                if arr[curr] == 0:
+                    curr += dir
+                else:
+                    arr[curr] -= 1
+                    dir = -dir
+                    curr += dir
+       
+            return all(x == 0 for x in arr)
+        
+        n = len(nums)
+        count = 0
+  
+        for i in range(n):
+            if nums[i] == 0:
+                if simulate(i, -1):
+                    count += 1
+                if simulate(i, 1):
+                    count += 1
+        
+        return count
