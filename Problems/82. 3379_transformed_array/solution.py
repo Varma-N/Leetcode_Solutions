8209021bc909c0ba4622a37ef198af77fdcf36b8
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-03-11 19:32:11 +0530

    Day 82: solved 3379 - Transformed Array

---
+++ Problems/82. 3379_transformed_array/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+
+        for i in range(n):
+            if nums[i] == 0:
+                result[i] = 0
+            else:
+                landing_index = (i + nums[i]) % n
+                result[i] = nums[landing_index]
+
+        return result
+        
