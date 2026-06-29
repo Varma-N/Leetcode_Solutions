c6f3e5eec286ee3a0940681768f71d5850fa5430
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-06-29 23:53:51 +0530

    Day 173: solved 61 - Rotate List

---
+++ Problems/173. 61_rotate_list/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+        old_tail = head
+        length = 1
+        while old_tail.next:
+            old_tail = old_tail.next
+            length += 1
+        old_tail.next = head
+        k = k % length
+        steps_to_new_tail = length - k - 1
+        
+        new_tail = head
+        for _ in range(steps_to_new_tail):
+            new_tail = new_tail.next
+        
+        new_head = new_tail.next
+       
+        new_tail.next = None
+        
+        return new_head
+            
+            
