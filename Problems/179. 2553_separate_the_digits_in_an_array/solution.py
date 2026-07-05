7fbc922e21ea007fca952ed006559430b8c330d9
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-07-05 11:37:16 +0530

    Day 179: solved 2553 - Separate the Digits in an Array

---
+++ Problems/179. 2553_separate_the_digits_in_an_array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        result = []
+        for num in nums:
+            result.extend(int(d) for d in str(num))
+        return result
+            
+
+            
