be044f0e2bc658cf4723ea9b445c1b2aebecd66e
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-07-07 19:43:41 +0530

    Day 181: solved 1674 - Minimum Moves to Make Array Complementary

---
+++ Problems/181. 1674_minimum_moves_to_make_array_complementary/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        delta = [0] * (2 * limit + 2)
+        
+        for i in range(n // 2):
+            A = min(nums[i], nums[n - 1 - i])
+            B = max(nums[i], nums[n - 1 - i])
+            delta[A + 1] -= 1
+            delta[A + B] -= 1
+            delta[A + B + 1] += 1
+            delta[B + limit + 1] += 1
+        curr_moves = n
+        min_moves = n
+        for x in range(2, 2 * limit + 1):
+            curr_moves += delta[x]
+            if curr_moves < min_moves:
+                min_moves = curr_moves
+                
+        return min_moves
+        
