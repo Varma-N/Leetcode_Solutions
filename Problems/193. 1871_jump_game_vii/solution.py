f22c20565004119711787455715866a8a40eb957
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-07-19 21:35:28 +0530

    Day 193: solved 1871 - Jump Game VII

---
+++ Problems/193. 1871_jump_game_vii/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if s[-1] == '1':
+            return False
+            
+        n = len(s)
+        reachable = [False] * n
+        reachable[0] = True
+        
+        active_jumps = 0
+        
+        for i in range(1, n):
+            if i >= minJump and reachable[i - minJump]:
+                active_jumps += 1
+            
+            if i > maxJump and reachable[i - maxJump - 1]:
+                active_jumps -= 1
+            
+            if active_jumps > 0 and s[i] == '0':
+                reachable[i] = True
+                
+        return reachable[-1]
+
+
+
+        
