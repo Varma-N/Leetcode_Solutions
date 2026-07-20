fe416f323c02742a8f75053837207306245e0f94
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-07-20 23:48:42 +0530

    Day 194: solved 3120 - Count the Number of Special Characters I

---
+++ Problems/194. 3120_count_the_number_of_special_characters_i/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        seen = set(word)
+        res = set()
+        for i in seen:
+            if i.upper() in seen and i.lower() in seen:
+                res.add(i.lower())
+        return len(res)
+
+        
