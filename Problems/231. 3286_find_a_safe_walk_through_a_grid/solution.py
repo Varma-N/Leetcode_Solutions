7fdaa9739a795131a216ad4b0e3a295c298c87ed
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-08-26 22:05:13 +0530

    Day 231: solved 3286 - Find a Safe Walk Through a Grid

---
+++ Problems/231. 3286_find_a_safe_walk_through_a_grid/solution.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        m, n = len(grid), len(grid[0])
+        dist = [[float('inf')] * n for _ in range(m)]
+        
+        dist[0][0] = grid[0][0]
+        q = deque([(0, 0)])
+        
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        while q:
+            r, c = q.popleft()
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    cost = grid[nr][nc]
+                    if dist[r][c] + cost < dist[nr][nc]:
+                        dist[nr][nc] = dist[r][c] + cost
+                        if cost == 0:
+                            q.appendleft((nr, nc))
+                        else:
+                            q.append((nr, nc))
+                            
+        return dist[m - 1][n - 1] < health
