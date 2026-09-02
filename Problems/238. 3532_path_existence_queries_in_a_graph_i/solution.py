431660a4c63977a3b4a41947a097ae58949a3ebb
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-09-02 22:31:47 +0530

    Day 238: solved 3532 - Path Existence Queries in a Graph I

---
+++ Problems/238. 3532_path_existence_queries_in_a_graph_i/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        components = [0] * n
+        curr_comp = 0
+        
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                curr_comp += 1
+            components[i] = curr_comp
+            
+        return [components[u] == components[v] for u, v in queries]
+        
