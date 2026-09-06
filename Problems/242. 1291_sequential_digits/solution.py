ff951a45d64a30b751172a8fdebc65d7526d7614
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-09-06 23:05:07 +0530

    Day 242: solved 1291 - Sequential Digits

---
+++ Problems/242. 1291_sequential_digits/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        sample = "123456789"
+        result = []
+        low_len = len(str(low))
+        high_len = len(str(high))
+        
+        for length in range(low_len, high_len+1):
+            for start in range(10-length):
+                num = int(sample[start:start+length])
+                if low <= num <= high:
+                    result.append(num)
+        return result
