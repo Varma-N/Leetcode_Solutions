f94513dd6e29a5f0ed9f21fb6e066619ab5439d0
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-09-08 23:30:00 +0530

    Day 244: solved 3658 - GCD of Odd and Even Sums

---
+++ Problems/244. 3658_gcd_of_odd_and_even_sums/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+        # def gcd(a, b):
+        #     while b:
+        #         a, b = b, a % b
+        #     return abs(a)
+
+        # odd_sum = 0
+        # even_sum = 0
+        # for i in range(1, (n * 2) + 1):
+        #     if i & 1:
+        #         odd_sum += i
+        #     else:
+        #         even_sum += i
+
+        # return gcd(odd_sum, even_sum)
+                
+
