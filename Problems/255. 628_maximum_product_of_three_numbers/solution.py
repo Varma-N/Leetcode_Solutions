7526d99ea1ec40639823d463e6a32758246191fe
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-09-20 00:43:59 +0530

    Day 255: solved 628 - Maximum Product of Three Numbers

--- Problems/255. 628_maximum_product_of_three_numbers/solution.py
+++ Problems/255. 628_maximum_product_of_three_numbers/solution.py
@@ -16,6 +16,6 @@ class Solution:
                 max3 = max2
                 max2 = n
             elif n >= max3:
-                max3 = n
+                max3 = n 
                 
         return max(min1 * min2 * max1, max1 * max2 * max3)
