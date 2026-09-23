e191f271a53ce0822f4e288600f71c79e48b054f
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2026-09-23 23:49:43 +0530

    Day 259: solved 3014 - Minimum Number of Pushes to Type Word I

--- Problems/259. 3014_minimum_number_of_pushes_to_type_word_i/solution.py
+++ Problems/259. 3014_minimum_number_of_pushes_to_type_word_i/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
         n = len(word)
-        return sum((i // 8) + 1 for i in range(n))  
+        return sum((i // 8) + 1 for i in range(n))
